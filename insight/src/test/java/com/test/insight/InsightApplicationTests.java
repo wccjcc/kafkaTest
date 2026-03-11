113fda52ee3e23074a3e2bcfa90882e61c4771b4
wccjcc <goqkfkrltl12@naver.com>
2026-03-11 21:49:38 +0900

    chore:project initialize

---
+++ insight/src/test/java/com/test/insight/InsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.insight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
